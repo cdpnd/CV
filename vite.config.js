8339b17274def86fb58fe98b0665a4ba1cb1372a
Eugene Pashchenko <eugene.pashchenko@gmail.com>
2025-06-04 14:04:25 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CV/",  // Change to match your repo name
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/index.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]"
+      }
+    }
+  },
   plugins: [react()]
 });
+
